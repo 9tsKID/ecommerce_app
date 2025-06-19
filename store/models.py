0788df7b9f12cefdd1889fa18cb7c1865589f3af
9tsKID <davidoloyede12@gmail.com>
2025-06-19 13:17:29 +0100

    Remove image field and switch fully to image_url

--- store/models.py
+++ store/models.py
@@ -24,7 +24,6 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image_url = models.URLField(blank=True, null=True)
-    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
